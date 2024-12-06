db38b2899f8c6edec300f152411eee34f91ce598
Timothy <timmytam10@gmail.com>
2024-12-06 20:58:30 +1100

    add landing and section

---
+++ showcase/components/project.js
@@ -0,0 +1,14 @@
+'use client';
+
+
+
+const Project = ({ title  }) => {
+
+    return (
+        <div  className="project">
+            <h2>{title}</h2>
+            <p>Development</p>
+        </div>
+    )
+}
+export default Project;
