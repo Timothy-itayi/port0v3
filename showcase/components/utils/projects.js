e6c10c1a4f84509abaa774c475f92a1e6131a620
Timothy <timmytam10@gmail.com>
2024-12-10 19:21:22 +1100

    add images for blog and update gph imgs

--- showcase/components/utils/projects.js
+++ showcase/components/utils/projects.js
@@ -10,7 +10,7 @@ export const projects = [
     {
       title: "GrandPrix History",
       description: "A web application made with NodeJs for the backend utilising OpenF1 API to access real-time Drivers standings and Positions along with a comprihensive  Grandpixs section detailing iconic Tracks and moments and finally an  Iconic Cars section utilising a narrated voice over explaining some details about the iconic cars.",
-      images: ["/projects-imgs/gph/gph-01.png", "/projects-imgs/gph/gph-02.png", "/projects-imgs/gph/gph-002.png", "/projects-imgs/gph/gph-004.png" ], 
+      images: ["/projects-imgs/gph/gph-000.png", "/projects-imgs/gph/gph-001.png","/projects-imgs/gph/gph-003.png", "/projects-imgs/gph/gph-002.png", ,"/projects-imgs/gph/gph-004.png" ], 
       link: "https://example.com/grandprix-history",
     },
     {
@@ -22,7 +22,7 @@ export const projects = [
     {
       title: "Timothy's Blog",
       description: "A personal blog where I share insights on web development.",
-      images:["/projects-imgs/gph/gph-01.png, /projects-imgs/gph/gph-001.png,/projects-imgs/gph/gph-002.png", "/projects-imgs/gph/gph-003.png"], 
+      images:["/projects-imgs/blog/blog-000.png"], 
       link: "https://example.com/blog",
     },
   
