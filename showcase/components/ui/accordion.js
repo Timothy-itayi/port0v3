b732aa4ca78de75544573a7d8706091c406bb885
Timothy <timmytam10@gmail.com>
2024-12-10 19:15:21 +1100

    update projects add waitless, gph , f1h

--- showcase/components/ui/accordion.js
+++ showcase/components/ui/accordion.js
@@ -24,7 +24,7 @@ const Accordion = ({ project, isOpen, toggleAccordion }) => {
   }, [isOpen]);
 
   return (
-    <div className=" col-span-1 mb-4 border-md text-gray-400 opacity-100 z-10 transition-all duration-300 hover:text-white  hover:opacity-100  overflow-hidden">
+    <div className=" col-span-1 mb-4 border-md text-gray-400 opacity-100 z-10 transition-all duration-300 hover:text-white  hover:opacity-100  overflow-hidden md:overflow-scroll">
     
    
       <header
@@ -50,11 +50,11 @@ const Accordion = ({ project, isOpen, toggleAccordion }) => {
       >
         <div 
           ref={contentRef}
-          className="p-4 bg-gray-100 text-black"
+          className=" header-font p-10  bg-black text-white  overflow-auto"
         >
         <p>{project.description}</p>
           {/* Render each image in the array */}
-          <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3">
+          <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 mt-10 ">
             {project.images.map((image, index) => (
               <Image
                 key={index}
@@ -62,7 +62,7 @@ const Accordion = ({ project, isOpen, toggleAccordion }) => {
                 alt={`${project.title} image ${index + 1}`}
                 width={500}  // You can adjust these values
                 height={500} // or set them dynamically if needed
-                className="rounded-lg object-cover"
+                className="rounded-lg object-cover mt-5 pb-5 "
               />
             ))}
           </div>
